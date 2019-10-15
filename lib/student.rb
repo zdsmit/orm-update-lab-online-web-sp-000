90f204f3a4df836861709ec8f95c45703ab52756
Zach  Smith <zdsmith001@gmail.com>
2019-10-15 16:15:16 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -50,8 +50,7 @@ class Student
     id = row[0]
     name = row[1]
     grade = row[2]
-    student = self.new(id, name, grade)
-    student
+    Student.new(id, name, grade)
   end
 
   def self.find_by_name
