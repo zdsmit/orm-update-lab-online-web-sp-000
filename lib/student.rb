8f0dfbd25abfe9cbc5b15619919fa6b5aa2487c0
Zach  Smith <zdsmith001@gmail.com>
2019-10-15 16:06:01 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -1,8 +1,7 @@
 require_relative "../config/environment.rb"
 
 class Student
-  attr_accessor :name, :grade
-  attr_reader :id
+  attr_accessor :name, :grade, :id
 
   # Remember, you can access your database connection anywhere in this class
   #  with DB[:conn]
