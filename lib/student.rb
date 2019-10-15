3a528758c3222312ed35b02517ab465aba91f6cd
Zach  Smith <zdsmith001@gmail.com>
2019-10-15 16:13:34 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -47,7 +47,10 @@ class Student
   end
 
   def self.new_from_db(row)
-    student = Student.new(row[0], row[1], row[2])
+    id = row[0]
+    name = row[1]
+    grade = row[2]
+    student = Student.new(id, name, grade)
     student
   end
 
